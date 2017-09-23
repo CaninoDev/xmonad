b6934b68ca154b7a886f3859f30d089130283d33
Canino Developments <ian.canino@gmail.com>
2017-09-23 05:19:21 -0700

    amended startuphook to add baseline init applications
    redshift-gtk-nm0applet, backlight

--- lib/XMonad/Custom/StartupHook.hs
+++ lib/XMonad/Custom/StartupHook.hs
@@ -1,7 +1,12 @@
 module XMonad.Custom.StartupHook where
 
 import           XMonad
--- | Perform an arbitrary action at xmonad startup.
-startupHook :: X ()
-startupHook = spawn "/home/caninodev/.local/bin/backlight"
+import           XMonad.Hooks.SetWMName
 
+
+	-- | Perform an arbitrary action at xmonad startup.
+startupHook :: X ()
+startupHook = setWMName "LG3d"
+            >> spawn "/home/caninodev/.local/bin/backlight"
+	          >> spawn "/usr/bin/nm-applet"
+		        >> spawn "/usr/bin/redshift-gtk"
