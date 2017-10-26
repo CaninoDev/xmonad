7255aad0f42f1b4689ef11b678ecf6417b09bff5
Canino Developments <ian.canino@gmail.com>
2017-10-26 14:11:26 -0700

    established dbus interface

--- .config/xmonad/lib/LogHook.hs
+++ .config/xmonad/lib/LogHook.hs
@@ -2,5 +2,50 @@ module LogHook
   ( myLogHook
   , connectDBusClient
   ) where
-myLogHook :: X ()
-mylogHook = return ()
+
+import qualified DBus as D
+import qualified DBus.Client as D
+import XMonad
+import XMonad.Hooks.EwmhDesktops (ewmhDesktopsLogHook)
+
+import XMonad.Hooks.DynamicLog
+
+import qualified Codec.Binary.UTF8.String as UTF8
+
+-- Define the specific paramenters for this dbus Connection
+dBusInterface = "user.xmonad.log"
+
+dBusPath = "/user/xmonad/log"
+
+dBusMember = "DynamicLogWithPP"
+
+myLogHook :: D.Client -> X ()
+myLogHook dbus = processDBusLog dbus
+
+-- Called in xmonad.hs, placed here for legibility
+connectDBusClient :: IO D.Client
+connectDBusClient = do
+  client <- D.connectSession
+  D.requestName client (D.busName_ dBusInterface) dBusParams
+  return client
+  where
+    dBusParams =
+      [D.nameAllowReplacement, D.nameReplaceExisting, D.nameDoNotQueue]
+
+-- This is the meat of the interface between dbus and xmonad via dynamicLogWithPP
+processDBusLog :: D.Client -> X ()
+processDBusLog dbusClient = do
+  ewmhDesktopsLogHook
+  dynamicLogWithPP $ def {ppOutput = signalDBus dbusClient}
+
+-- Emit a DBus signal on log updates
+signalDBus :: D.Client -> String -> IO ()
+signalDBus client message = do
+  let signal =
+        (D.signal objectPath interfaceName memberName)
+        {D.signalBody = [D.toVariant $ UTF8.decodeString message]}
+  D.emit client signal
+  where
+    objectPath = D.objectPath_ dBusPath
+    interfaceName = D.interfaceName_ dBusInterface
+    memberName = D.memberName_ dBusMember
--- .config/xmonad/xmonad.hs
+++ .config/xmonad/xmonad.hs
@@ -1,10 +1,10 @@
 {-# OPTIONS -fno-warn-missing-signatures -ilib #-}
 
 import XMonad
+import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.EwmhDesktops (ewmh)
 import XMonad.Hooks.ManageDocks
 
-{- import XMonad.Hooks.UrgencyHook (UrgencyHook, urgencyHook, withUrgencyHook) -}
 import KeysMouseBindings
 import Layout
 import LogHook
@@ -15,23 +15,25 @@ import qualified DBus as D
 import qualified DBus.Client as D.Client
 
 main :: IO ()
-main =
-    xmonad $ ewmh $ docks myConfig  -- Add docks functionality to the given config
+main = do
+  dbusLine <- connectDBusClient -- connect to dbus to interface with statusbars and pass on the connection to LogHook
+  xmonad $ ewmh $ myConfig dbusLine
 
-myConfig = def
-  { borderWidth               = myBorderWidth
-    , workspaces              = myWorkspaces
-    , layoutHook              = avoidStruts myLayout          -- Adjust layout automagically to no voer up any dock, status bars, etc...
-    , terminal                = myTerminal
-    , normalBorderColor       = myNormalBorderColor
-    , focusedBorderColor      = myFocusedBorderColor
-    , modMask                 = myDefaultModMask
-    , keys                    = myKeys
-    , logHook                 = myLogHook
-    , startupHook             = myStartupHook
-    , mouseBindings           = myMouseBindings
-    , manageHook              = myManageHook
-    , focusFollowsMouse       = myFocusFollowsMouse
-    , clickJustFocuses        = myClickJustFocuses
-    , handleEventHook         = docksEventHook              -- Whenever a new dock appears, refresh the layout immediately to avoid the new dock
+myConfig dbusLine =
+  def
+  { borderWidth = myBorderWidth
+  , workspaces = myWorkspaces
+  , layoutHook = avoidStruts myLayout -- Adjust layout automagically to no cover up any dock, status bars, etc...
+  , terminal = myTerminal
+  , normalBorderColor = myNormalBorderColor
+  , focusedBorderColor = myFocusedBorderColor
+  , modMask = myDefaultModMask
+  , keys = myKeys
+  , logHook = myLogHook dbusLine
+  , startupHook = myStartupHook
+  , mouseBindings = myMouseBindings
+  , manageHook = myManageHook
+  , focusFollowsMouse = myFocusFollowsMouse
+  , clickJustFocuses = myClickJustFocuses
+  , handleEventHook = docksEventHook -- Whenever a new dock appears, refresh the layout immediately to avoid the new dock
   }
