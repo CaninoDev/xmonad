0d89c4808bec3f3a86918118dfaf2cac042ac7f4
Canino Developments <ian.canino@gmail.com>
2017-10-27 02:44:43 -0700

    added keyboard shortcut for controlling monitor brightness

--- .config/xmonad/lib/KeysMouseBindings.hs
+++ .config/xmonad/lib/KeysMouseBindings.hs
@@ -5,6 +5,7 @@ import XMonad
 import XMonad.Prompt
 import XMonad.Prompt.Shell hiding (shellPrompt)
 import qualified XMonad.StackSet as W
+import Graphics.X11.ExtraTypes.XF86
 
 import PromptConfig
        (brwsrPrompt, myBrwsrConfig, mySrchConfig, myXPConfig, shellPrompt)
@@ -42,6 +43,7 @@ myDefaultModMask = mod4Mask -- The command key on Macbook Pro
 myKeys :: XConfig Layout -> M.Map (KeyMask, KeySym) (X ())
 myKeys conf@(XConfig {XMonad.modMask = modMask}) =
   M.fromList $
+
     -- launching and killing programs
   [ ((modMask .|. shiftMask, xK_Return), spawn $ XMonad.terminal conf) -- %! Launch terminal
   , ((modMask, xK_p), shellPrompt myXPConfig)
@@ -51,24 +53,33 @@ myKeys conf@(XConfig {XMonad.modMask = modMask}) =
   , ((modMask, xK_space), sendMessage NextLayout) -- %! Rotate through the available layout algorithms
   , ((modMask .|. shiftMask, xK_space), setLayout $ XMonad.layoutHook conf) -- %!  Reset the layouts on the current workspace to default
   , ((modMask, xK_n), refresh) -- %! Resize viewed windows to the correct size
+
     -- move focus up or down the window stack
   , ((modMask, xK_Tab), windows W.focusDown) -- %! Move focus to the next window
   , ((modMask .|. shiftMask, xK_Tab), windows W.focusUp) -- %! Move focus to the previous window
   , ((modMask, xK_j), windows W.focusDown) -- %! Move focus to the next window
   , ((modMask, xK_k), windows W.focusUp) -- %! Move focus to the previous window
   , ((modMask, xK_m), windows W.focusMaster) -- %! Move focus to the master window
+
     -- modifying the window order
   , ((modMask, xK_Return), windows W.swapMaster) -- %! Swap the focused window and the master window
   , ((modMask .|. shiftMask, xK_j), windows W.swapDown) -- %! Swap the focused window with the next window
   , ((modMask .|. shiftMask, xK_k), windows W.swapUp) -- %! Swap the focused window with the previous window
+
     -- resizing the master/slave ratio
   , ((modMask, xK_h), sendMessage Shrink) -- %! Shrink the master area
   , ((modMask, xK_l), sendMessage Expand) -- %! Expand the master area
+
     -- floating layer support
   , ((modMask, xK_t), withFocused $ windows . W.sink) -- %! Push window back into tiling
+
     -- increase or decrease number of windows in the master area
   , ((modMask, xK_comma), sendMessage (IncMasterN 1)) -- %! Increment the number of windows in the master area
   , ((modMask, xK_period), sendMessage (IncMasterN (-1))) -- %! Deincrement the number of windows in the master area
+  -- multimedia keys
+  -- control monitor brightness
+  , ((0, xF86XK_MonBrightnessUp), spawn "xbacklight -inc 10 -steps 1024 -time 80")
+  , ((0, xF86XK_MonBrightnessDown), spawn "xbacklight -dec 10 -steps 1024 -time 80")
     -- quit, or restart
   , ((modMask .|. shiftMask, xK_q), io (exitWith ExitSuccess)) -- %! Quit xmonad
   , ( (modMask, xK_q)
